17db207f2a780a89dc6e50ea1e0bcdc8e886b6ba
Mengqi Wu <pwu052@uottawa.ca>
2019-12-01 12:12:44 -0500

    Initial setup for lambda

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 import * as Alexa from 'ask-sdk';
+import { HandlerInput, RequestHandler } from 'ask-sdk-core';
+import { Response } from 'ask-sdk-model';
+
+const OrderFoodHandler: RequestHandler = {
+    canHandle(handlerInput: HandlerInput): boolean {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'OrderFoodIntent';
+    },
+    handle(handlerInput: HandlerInput): Response {
+
+        const speechText = 'Hello World!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    },
+}
 
 exports.handler = async (event) => {
     // TODO implement
