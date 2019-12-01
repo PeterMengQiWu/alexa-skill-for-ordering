f33e4f3a2662cf89ce2e2e243b739a6015728c6d
Mengqi Wu <pwu052@uottawa.ca>
2019-12-01 11:23:41 -0500

    Setting up typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as Alexa from 'ask-sdk';
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
